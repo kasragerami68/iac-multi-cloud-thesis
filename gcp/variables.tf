8c22ea3a84b7da8bafb281accd907fbd590324fd
kasragerami68 <kasragerami@gmail.com>
2025-12-07 13:26:47 +0000

    create vaiables

---
+++ gcp/variables.tf
@@ -0,0 +1,59 @@
+variable "project_id" {
+  description = "GCP Project ID"
+  type        = string
+}
+
+variable "project_name" {
+  description = "Project name prefix"
+  type        = string
+  default     = "iac-web-app"
+}
+
+variable "region" {
+  description = "GCP region"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "zone" {
+  description = "GCP zone"
+  type        = string
+  default     = "us-central1-a"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "dev"
+}
+
+variable "machine_type" {
+  description = "Compute Engine machine type"
+  type        = string
+  default     = "e2-micro"
+}
+
+variable "db_tier" {
+  description = "Cloud SQL instance tier"
+  type        = string
+  default     = "db-f1-micro"
+}
+
+variable "db_name" {
+  description = "Database name"
+  type        = string
+  default     = "community_db"
+}
+
+variable "db_user" {
+  description = "Database username"
+  type        = string
+  default     = "kasra"
+}
+
+variable "db_password" {
+  description = "Database password"
+  type        = string
+  default     = "test1234"
+  sensitive   = true
+}
