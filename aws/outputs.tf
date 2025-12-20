2ad01dcdb0e59bcdc33a934ce98810e156e46af7
Kasra Gerami <kasragerami68@gmail.com>
2025-12-20 11:15:17 +0000

    Trigger CI/CD - Add test comment to AWS outputs

--- aws/outputs.tf
+++ aws/outputs.tf
@@ -28,10 +28,7 @@ output "web_server_availability_zone" {
   description = "Server availability zone"
   value       = aws_instance.web.availability_zone
 }
-output "application_url" {
-  description = "URL to access the web application"
-  value       = "http://${aws_eip.web.public_ip}:9090"
-}
+
 # ====================================================================
 # Database Information
 # ====================================================================
