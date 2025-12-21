6d930bf673dde5033d9025dc82ddd8ff033dca51
Kasra Gerami <kasragerami68@gmail.com>
2025-12-21 09:35:27 +0000

    Add remote state backend with S3 and DynamoDB

--- aws/providers.tf
+++ aws/providers.tf
@@ -3,6 +3,13 @@
 terraform {
   required_version = ">= 1.0"
   
+    backend "s3" {
+    bucket         = "iac-thesis-terraform-state-397245851405"
+    key            = "aws/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-locks"
+    encrypt        = true
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
--- gcp/providers.tf
+++ gcp/providers.tf
@@ -1,5 +1,12 @@
 ﻿terraform {
   required_version = ">= 1.0"
+  backend "s3" {
+    bucket         = "iac-thesis-terraform-state-397245851405"
+    key            = "gcp/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-locks"
+    encrypt        = true
+  }
   required_providers {
     google = {
       source  = "hashicorp/google"
